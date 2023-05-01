49a74dac84aa05ecd9d7680d3a34e34d5fe1aa91
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-05-01 18:09:18 +0200

    more dependancy | actions types

---
+++ src/state/reducers/bankReducer.ts
@@ -0,0 +1,17 @@
+import { Action } from "../actions";
+const initialState = 0;
+
+const reducer = (state: number = initialState, action: Action) => {
+        switch (action.type){
+            case "deposit":
+                return state + action.payload
+            case "withdraw":
+                return state - action.payload
+            case "bankrupt":
+                return 0
+            default:
+                return state
+        }
+}
+
+export default reducer
